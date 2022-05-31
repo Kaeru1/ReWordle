ab2e63c5e149a4c7a9e255e59714d87fd4d8de8a
Kaeru <jayden.beck@aotawhiti.school.nz>
2022-06-01 10:52:55 +1200

    made canvas display/highlight letters

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ let answer;
 
 // resets/initalizes variables
 function reset(){
+    ctx.fillStyle = "white"
+    ctx.fillRect(0, 0, 150, 150)
     answer = wordlist[Math.floor(Math.random()*wordlist.length)];
     atempt = 0;
     for(x=0; x<6; x++){
@@ -28,22 +30,40 @@ function guess(word = document.getElementById("input").value){
     if(inWordlist){
         atempt += 1;
         if(word === answer){
-            console.log("correct");
+
+            ctx.fillStyle = "green";
+            ctx.fillRect(0, atempt*24-24, 150,24);
+
+            for(x=0;x<5;x++){
+                ctx.fillStyle = "black"
+                ctx.font = "20px Arial";
+                ctx.fillText(word[x], x*30+5 , atempt*24);
+                console.log("correct");
+            }
         } else {
-            let placement = ["not","not","not","not","not"];
+
+            let placement = ["lightgray","lightgray","lightgray","lightgray","lightgray"];
             for(x=0; x<5; x++){
+
                 for(y=0; y<5; y++){
                     if(word[x] === answer[y]){
-                        placement[x] = "move";
+                        placement[x] = "yellow";
                     };
                 };
 
                 if(word[x] === answer[x]){
-                    placement[x] = "yes";
+                    placement[x] = "green";
                 };
+
+                ctx.fillStyle = placement[x];
+                ctx.fillRect(x*30, atempt*24-24, 30,24);
+
+                ctx.fillStyle = "black"
+                ctx.font = "20px Arial";
+                ctx.fillText(word[x], x*30+5 , atempt*24);
             };
-            ctx.font = "30px Arial";
-            ctx.fillText(placement, 10 , atempt*24)
+            //ctx.font = "20px Arial";
+            //ctx.fillText(placement, 10 , atempt*24);
             console.log(placement);
         };
     } else {
