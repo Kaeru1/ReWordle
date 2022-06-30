44e7cb3db9cf691aae1f20a3c0dd4012ae732e4b
Kaeru <jayden.beck@aotawhiti.school.nz>
2022-07-01 10:01:38 +1200

    Display possible anwers

--- main.js
+++ main.js
@@ -2,82 +2,126 @@
 // variables
 var ctx = document.getElementById("canvas").getContext("2d");
 const wordlist = advanced;
-let attepts = [null,null,null,null,null,null];
+const answerList = intermediate;
+let possibleAnswerList = answerList;
+let attempts = [null,null,null,null,null,null];
 let failed = false;
-let atempt;
+let attempt;
 let answer;
 
 // resets/initalizes variables
 function reset(){
-    ctx.fillStyle = "white"
-    ctx.fillRect(0, 0, 150, 150)
-    answer = wordlist[Math.floor(Math.random()*wordlist.length)];
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, 150, 150);
+    answer = answerList[Math.floor(Math.random()*answerList.length)];
+    possibleAnswerList = answerList;
     failed = false;
-    atempt = 0;
+    attempt = 0;
     for(x=0; x<6; x++){
-        attepts[x] = null;
+        attempts[x] = null;
     };
 };
 
-// prosseses guess and returns placement
-function guess(word = document.getElementById("input").value.toUpperCase()){
+// prosseses guess and returns letterHighlights
+function guess(guessedWord = document.getElementById("input").value.toUpperCase()){
     document.getElementById("input").value = "";
-    console.log(word)
-    console.log(answer)
+    document.getElementById("tempId").innerText = "";
+    console.log(guessedWord);
+    console.log(answer);
     let inWordlist = false;
     for(x in wordlist){
-        if(word == wordlist[x]){
+        if(guessedWord == wordlist[x]){
             inWordlist = true;
         };
     };
+    // checks if guessedWord is in wordlist and if player has failed
     if(inWordlist && !failed){
-        atempt += 1;
-        if(word == answer){
+        attempt += 1;
+        document.getElementById("attempt").innerText = attempt;
+        if(guessedWord == answer){
 
             ctx.fillStyle = "green";
-            ctx.fillRect(0, atempt*24-24, 150,24);
+            ctx.fillRect(0, attempt*24-24, 150,24);
 
             for(x=0;x<5;x++){
-                ctx.fillStyle = "black"
+                ctx.fillStyle = "black";
                 ctx.font = "20px Arial";
-                ctx.fillText(word[x], x*30+5 , atempt*24);
+                ctx.fillText(guessedWord[x], x*30+5 , attempt*24);
                 console.log("correct");
-            }
+            };
         } else {
 
-            let placement = ["lightgray","lightgray","lightgray","lightgray","lightgray"];
+            let letterHighlights = ["lightgray","lightgray","lightgray","lightgray","lightgray"];
             for(x=0; x<5; x++){
-
                 for(y=0; y<5; y++){
-                    if(word[x] === answer[y]){
-                        placement[x] = "yellow";
+                    if(guessedWord[x] === answer[y]){
+                        letterHighlights[x] = "yellow";
                     };
                 };
 
-                if(word[x] === answer[x]){
-                    placement[x] = "green";
+                if(guessedWord[x] === answer[x]){
+                    letterHighlights[x] = "green";
                 };
 
-                ctx.fillStyle = placement[x];
-                ctx.fillRect(x*30, atempt*24-24, 30,24);
+                ctx.fillStyle = letterHighlights[x];
+                ctx.fillRect(x*30, attempt*24-24, 30,24);
 
-                ctx.fillStyle = "black"
+                ctx.fillStyle = "black";
                 ctx.font = "20px Arial";
-                ctx.fillText(word[x], x*30+5 , atempt*24);
+                ctx.fillText(guessedWord[x], x*30+5 , attempt*24);
             };
-            console.log(placement);
-            if(atempt == 6){
+            if(attempt == 6){
                 console.log("you lost");
                 failed = true;
-            }
+            };
+            checkPossibleAnswers(letterHighlights, guessedWord);
         };
     } else {
         if(failed){
-            console.log("you have no more atempts")
+            document.getElementById("tempId").innerText = "you have no more attempts";
         } else {
-            console.log("not in wordlist");
-        }
+            document.getElementById("tempId").innerText = "not in wordlist";
+        };
     };
+};
+
+function checkPossibleAnswers(letterHighlights,guessedWord){
+    let possibleAnswers = 0;
+    for(z in possibleAnswerList){
+            if(possibleAnswerList[z] != null){
+            let tempName = ["lightgray","lightgray","lightgray","lightgray","lightgray"];
+            for(x=0; x<5; x++){
+                for(y=0; y<5; y++){
+                    if(guessedWord[x] === possibleAnswerList[z][x]){
+                        tempName[x] = "yellow";
+                    };
+                };
+
+                if(guessedWord[x] === possibleAnswerList[z][x]){
+                    tempName[x] = "green";
+                };
+            };
+            let keepAsPossibleAnswer = false;
+            if(letterHighlights[0] == tempName[0]){
+                if(letterHighlights[1] == tempName[1]){
+                    if(letterHighlights[2] == tempName[2]){
+                        if(letterHighlights[3] == tempName[3]){
+                            if(letterHighlights[4] == tempName[4]){
+                                console.log(possibleAnswerList[z]);
+                                keepAsPossibleAnswer = true;
+                                possibleAnswers += 1;
+                            };
+                        };
+                    };
+                };
+            };
+            if(!keepAsPossibleAnswer){
+                possibleAnswerList[z] = null;   
+            };
+        };
+    };
+    console.log(`There are ${possibleAnswers} possible answers left`);
+    document.getElementById("possible answers").innerText = `There are ${possibleAnswers} possible answers left`;
 };
 
 reset();
